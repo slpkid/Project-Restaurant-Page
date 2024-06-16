249d860dd1138c64c3f568010f50aabc95e9b33b
slpkid <153894144+slpkid@users.noreply.github.com>
2024-06-16 13:04:49 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Project Restaurant',
+        filename: 'index.html',
+        inject: 'body',
+        template: './src/index.html'
+    }),
+  ],
+};
